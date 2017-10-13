d1a2b2e6a5fdd81151f809a86c191c0b1123e949
LinaShadrach <lina.shadrach@gmail.com>
2017-10-13 14:25:28 -0700

    add propTpes to project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Project(props){
   return (
-    <h4>Knerdy Knitter</h4>
+    <div>
+      <h4>{props.title}</h4>
+      <h6>{props.description}</h6>
+      <p>{props.technologies}</p>
+    </div>
   );
 }
+Project.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  technologies: PropTypes.string
+};
 
 export default Project;
--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -3,7 +3,10 @@ import Project from "./Project"
 
 function ProjectList(props){
   return (
-    <Project/>
+    <Project
+      title="Knerdy Knitter"
+      description="generates complex random knitting patterns according to simple rules"
+      technologies="AJAX, C# 6, CSS, D3, HTML5, ES5, Materialize, MSSQL, .NET Core 1.0 (with Entity and Identity), SSMS, VS 2015"/>
   );
 }
 
