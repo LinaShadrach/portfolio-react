32aa4b82a33aa7277e9e148933f75bec60826a79
LinaShadrach <lina.shadrach@gmail.com>
2017-10-13 13:22:41 -0700

    add barebones index files

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Lina Shadrach</h1>
+    <h3>Web Developer</h3>
+  </div>,
+  document.getElementById('react-app-root')
+);
