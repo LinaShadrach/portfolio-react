f017b5f44e24c868a9adf425283021d14888b5ff
LinaShadrach <lina.shadrach@gmail.com>
2018-03-17 15:13:26 -0700

    configure webpack to use esLint loder and style-jsx

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,16 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
+        enforce: "pre",
+        loader: "eslint-loader",
+        exclude: /node_modules/,
+        options: {
+          emitWarning: true,
+          configFile: "./.eslintrc.json"
+          }
+      },
+      {
+        test: /\.jsx?$/,
         loader: "babel-loader",
         exclude: /node_modules/,
         options: {
@@ -37,7 +47,8 @@ module.exports = {
             "react"
           ],
           plugins: [
-            "react-hot-loader/babel"
+            "react-hot-loader/babel",
+            "styled-jsx/babel"
           ]
         }
       }
