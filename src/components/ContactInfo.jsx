11a6637922d4cad1eae957af554b282fb9301109
LinaShadrach <lina.shadrach@gmail.com>
2018-03-17 11:57:29 -0700

    create ContactInfo component

---
+++ src/components/ContactInfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ContactInfo(){
+  return (
+    <div>
+      <a href="github.com/LinaShadrach">Github</a>
+      <a href="https://www.linkedin.com/in/lina-shadrach/">LinkedIn</a>
+    </div>
+  );
+}
+
+export default ContactInfo;
