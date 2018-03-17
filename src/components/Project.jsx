fe4998b0c86541902d3a737052b219ea950b3784
LinaShadrach <lina.shadrach@gmail.com>
2018-03-17 12:47:54 -0700

    add links to github projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,7 +5,9 @@ import { Col } from 'react-materialize';
 function Project(props){
   return (
     <Col s={12} m={4} l={4}>
-      <h4>{props.title}</h4>
+      <a href={props.ghUrl} target='_blank'>
+        <h4>{props.title}</h4>
+      </a>
       <h6>{props.description}</h6>
       <p>{props.technologies}</p>
     </Col>
@@ -14,7 +16,8 @@ function Project(props){
 Project.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
-  technologies: PropTypes.string
+  technologies: PropTypes.string,
+  ghUrl: PropTypes.string
 };
 
 export default Project;
--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -6,17 +6,20 @@ var masterProjectList = [
   {
     title: 'Knerdy Knitter',
     description: 'generates complex random knitting patterns according to simple rules',
-    tech: 'AJAX, C# 6, CSS, D3, HTML5, ES5, Materialize, MSSQL, .NET Core 1.0 (with Entity and Identity), SSMS, VS 2015'
+    tech: 'AJAX, C# 6, CSS, D3, HTML5, ES5, Materialize, MSSQL, .NET Core 1.0 (with Entity and Identity), SSMS, VS 2015',
+    ghUrl: 'https://github.com/LinaShadrach/KnerdyKnitter'
   },
   {
     title: 'Sleater and Kinney',
     description: '4B',
-    tech: 'Fox image not displaying on page, can only see duck?'
+    tech: 'Fox image not displaying on page, can only see duck?',
+    ghUrl: 'https://github.com/LinaShadrach/blogsite'
   },
   {
     title: 'Imani & Jacob',
     description: '9F',
-    tech: 'Donkey picture not displaying on hover in Zoology app. :('
+    tech: 'Donkey picture not displaying on hover in Zoology app. :(',
+    ghUrl: 'https://github.com/LinaShadrach/tune-trip'
   }
 ];
 
@@ -30,6 +33,7 @@ function ProjectList(){
           title={project.title}
           description={project.description}
           tech={project.tech}
+          ghUrl={project.ghUrl}
           key={index}
         />
       )}
