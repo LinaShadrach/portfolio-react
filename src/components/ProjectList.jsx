60068beda0313c55eb3aa7d7596b3fa329a3a33f
LinaShadrach <lina.shadrach@gmail.com>
2018-03-18 02:27:34 -0700

    extract master project list

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -2,31 +2,10 @@ import React from 'react';
 import Project from './Project';
 import { Row } from 'react-materialize';
 import { panel } from './../styles/styles.js';
-
-var masterProjectList = [
-  {
-    title: 'Knerdy Knitter',
-    description: 'generates complex random knitting patterns according to simple rules',
-    tech: 'AJAX, C# 6, CSS, D3, HTML5, ES5, Materialize, MSSQL, .NET Core 1.0 (with Entity and Identity), SSMS, VS 2015',
-    ghUrl: 'https://github.com/LinaShadrach/KnerdyKnitter'
-  },
-  {
-    title: 'Sleater and Kinney',
-    description: '4B',
-    tech: 'Fox image not displaying on page, can only see duck?',
-    ghUrl: 'https://github.com/LinaShadrach/blogsite'
-  },
-  {
-    title: 'Imani & Jacob',
-    description: '9F',
-    tech: 'Donkey picture not displaying on hover in Zoology app. :(',
-    ghUrl: 'https://github.com/LinaShadrach/tune-trip'
-  }
-];
-
+import { masterProjectList } from './../data/master-project-list.js';
 function ProjectList(){
   return (
-    <Row>
+    <div>
       <style jsx>{panel}</style>
       <div id='projectspy' className='panel deep-purple lighten-4'>
         <Row>
@@ -43,7 +22,7 @@ function ProjectList(){
           )}
         </Row>
       </div>
-    </Row>
+    </div>
   );
 }
 
