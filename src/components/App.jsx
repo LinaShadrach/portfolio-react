95780566e80ed03f2900037b93474888106cccf3
LinaShadrach <lina.shadrach@gmail.com>
2018-03-18 02:29:04 -0700

    replace components with MyContent

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import Header from './Header';
-import ProjectList from './ProjectList';
-import Experience from './Experience';
-import SkillsGroup from './SkillsGroup';
-import ContactInfo from './ContactInfo';
+import MyContent from './MyContent';
 import { Parallax } from 'react-materialize';
 import { Row } from 'react-materialize';
 import { fontStyles } from './../styles/styles.js';
@@ -15,10 +12,7 @@ function App(){
       <Header></Header>
       <Parallax imageSrc="https://scontent.fsnc1-1.fna.fbcdn.net/v/t1.0-9/12923178_10206186647568769_454637490761351205_n.jpg?oh=3cca0a314d15c16a6e6bc9affe6809e6&oe=5B2DB964">
       </Parallax>
-      <Experience></Experience>
-      <ProjectList></ProjectList>
-      <SkillsGroup></SkillsGroup>
-      <ContactInfo></ContactInfo>
+      <MyContent></MyContent>
     </Row>
   );
 }
