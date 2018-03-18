2888f4f4104ecc9710fb7e78ba4721bf6ed5f208
LinaShadrach <lina.shadrach@gmail.com>
2018-03-17 23:37:56 -0700

    create 2 remaining skill lists

--- src/data/master-skills-list.js
+++ src/data/master-skills-list.js
@@ -1,12 +1,50 @@
-export const masterSkillsList = [
+export const masterJsSkillsList = [
   {
-    title: 'C#'
+    title: 'Javascript',
+    description: '2 years'
   },
   {
-    title: 'ASP.NET'
-
+    title: 'Angular',
+    description: 'Angular 2-5, Angular CLI'
   },
   {
-    title: 'VS 2015, 2017'
+    title: 'React',
+    description: 'This site was built with React!'
+  },
+  {
+    title: 'jQuery',
+    description: '2 years'
+  }
+];
+export const masterCSkillsList = [
+  {
+    title: 'C#',
+    description: '2 years'
+  },
+  {
+    title: 'ASP.NET Core',
+    description: 'MVC, Identity, Entity Framework'
+  },
+  {
+    title: 'VS 2015, 2017',
+    description: 'Developed on Windows & Mac'
+  }
+];
+export const masterOtherSkillsList = [
+  {
+    title: 'Git',
+    description: '2 years'
+  },
+  {
+    title: 'SQL',
+    description: 'MSSQL with SSMS & MySQL with MAMP'
+  },
+  {
+    title: 'NoSql',
+    description: 'Firebase'
+  },
+  {
+    title: 'CSS & SASS',
+    description: 'Materialize.css, Bootstrap.css, Tuxedo.css'
   }
 ];
