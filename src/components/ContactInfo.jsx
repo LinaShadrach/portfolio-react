27850b7212dba69fe2e0a06c67e99cd83e4364bb
LinaShadrach <lina.shadrach@gmail.com>
2018-03-17 22:21:23 -0700

    add icons for contact info

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Row } from 'react-materialize';
+import { Row, Col, Icon } from 'react-materialize';
+import { contactInfoStyles } from './../styles/styles.js';
 function ContactInfo(){
   return (
-    <Row className="center">
-      <div>
-        <div id="contactspy">
-          <a  href="github.com/LinaShadrach">Github</a>
-          <a  href="https://www.linkedin.com/in/lina-shadrach/" >LinkedIn</a>
-        </div>
-      </div>
+    <Row>
+      <style jsx>{contactInfoStyles}</style>
+      <Row id="contactspy"  className="center">
+        <Col s={4} m={4} l={4}>
+          <a href="github.com/LinaShadrach"><img className="contact-icon" src='https://image.flaticon.com/icons/svg/25/25231.svg'></img></a>
+        </Col>
+        <Col s={4} m={4} l={4}>
+          <a href='mailto:lina@epicodus.com'><Icon medium>email</Icon></a>
+        </Col>
+        <Col s={4} m={4} l={4}>
+          <a  href="https://www.linkedin.com/in/lina-shadrach/" ><img className="contact-icon" src='https://cdn3.iconfinder.com/data/icons/free-social-icons/67/linkedin_circle_black-512.png'></img></a>
+        </Col>
+      </Row>
     </Row>
   );
 }
