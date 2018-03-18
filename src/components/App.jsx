4bcd19d9c6d936234c7568f289b0bd21ade9be3e
LinaShadrach <lina.shadrach@gmail.com>
2018-03-17 17:47:44 -0700

    add parallax

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,15 @@ import ProjectList from './ProjectList';
 import Experience from './Experience';
 import SkillsGroup from './SkillsGroup';
 import ContactInfo from './ContactInfo';
+import { Parallax } from 'react-materialize';
 import { Row } from 'react-materialize';
 
 function App(){
   return (
     <Row>
-      <Header/>
+      <Header></Header>
+      <Parallax imageSrc="https://scontent.fsnc1-1.fna.fbcdn.net/v/t1.0-9/12923178_10206186647568769_454637490761351205_n.jpg?oh=3cca0a314d15c16a6e6bc9affe6809e6&oe=5B2DB964">
+      </Parallax>
       <ProjectList></ProjectList>
       <Experience></Experience>
       <SkillsGroup></SkillsGroup>
