6b2f96b9ee907adda887b83f187af39fc0938f37
LinaShadrach <lina.shadrach@gmail.com>
2018-03-17 21:46:02 -0700

    remove scrollspy class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,10 +13,10 @@ function App(){
       <Header></Header>
       <Parallax imageSrc="https://scontent.fsnc1-1.fna.fbcdn.net/v/t1.0-9/12923178_10206186647568769_454637490761351205_n.jpg?oh=3cca0a314d15c16a6e6bc9affe6809e6&oe=5B2DB964">
       </Parallax>
-      <Experience className="scrollspy"></Experience>
-      <ProjectList className="scrollspy"></ProjectList>
-      <SkillsGroup className="scrollspy"></SkillsGroup>
-      <ContactInfo className="scrollspy"></ContactInfo>
+      <Experience></Experience>
+      <ProjectList></ProjectList>
+      <SkillsGroup></SkillsGroup>
+      <ContactInfo></ContactInfo>
     </Row>
   );
 }
