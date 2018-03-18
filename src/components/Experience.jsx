4ebeeeafed0a90c96fd4656d843f13c590071a28
LinaShadrach <lina.shadrach@gmail.com>
2018-03-17 21:13:56 -0700

    remove extra space above experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Row } from 'react-materialize';
-import { CardPanel } from 'react-materialize';
 import { panel } from './../styles/styles.js';
 function Experience(){
   return (
     <Row>
       <style jsx>{panel}</style>
-      <div id='experiencespy' className='panel'>
-        <CardPanel className="deep-purple lighten-4">
+      <div className='panel'>
+        <div  id='experiencespy' className="deep-purple lighten-4">
           <h3 className='center'>Epicodus</h3>
           <p>My journey.</p>
           <p><a href='epicodus.com'>Epicodus</a></p>
-        </CardPanel>
+        </div>
       </div>
     </Row>
   );
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -4,5 +4,10 @@ export let panel = css`
   .panel {
     margin: auto;
   }
-
+  #experiencespy {
+    transition: box-shadow .25s;
+    padding: 24px;
+    margin: 0;
+    border-radius: 2px;
+  }
 `;
